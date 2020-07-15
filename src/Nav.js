3d42e5fa2e3bdbc83392ddf4b551e0d3215c3032
“Togara <togarahess@gmail.com>
2020-07-15 15:20:24 +0200

    My first TogaraFlix commit

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/o/"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/12401155"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
